4c17a1fe32a22498dbdf2db6059d04eaa93e3598
Aleksey Vakht <AlekseyVakht@yandex.ru>
2023-06-10 11:43:33 +0300

    ver 1.1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
-import './index.css';
-import ImagePopup from './components/ImagePopup';
-import Header from './components/Header';
-import Main from './components/Main';
-import Footer from './components/Footer';
-import PopupWithForm from './components/PopupWithForm';
+import '../index.css';
+import ImagePopup from './ImagePopup';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+import PopupWithForm from './PopupWithForm';
 
 function App() {
   const [isEditProfilePopupOpen, setEditProfilePopupOpen] = useState(false)
